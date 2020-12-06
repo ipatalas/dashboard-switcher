961bb2d4eb192af9834c018ac30645b60269c9da
Ireneusz Patalas <fido@debian.local>
2020-12-06 21:50:31 +0100

    Add support for week days

--- dashboard.js
+++ dashboard.js
@@ -5,6 +5,7 @@ export class Dashboard {
      * @param {string} schedule.url
      * @param {string} schedule.duration
      * @param {string} schedule.active_hours
+     * @param {Array<string>} schedule.weekdays
      */
     constructor(schedule) {
         this.url = schedule.url;
@@ -13,6 +14,7 @@ export class Dashboard {
         const active_hours = (schedule.active_hours || '00:00-23:59').split('-');
         this.activeFrom = parseHourAndMinutes(active_hours[0]);
         this.activeTo = parseHourAndMinutes(active_hours[1]);
+        this.activeWeekday = schedule.weekdays || ['all'];
 
         this.isOvernight = this.activeFrom.h > this.activeTo.h;
 
@@ -28,10 +30,29 @@ export class Dashboard {
             const isAfterFrom = h > this.activeFrom.h || h == this.activeFrom.h && m >= this.activeFrom.m;
             const isBeforeTo = h < this.activeTo.h || h == this.activeTo.h && m <= this.activeTo.m;
 
-            return this.isOvernight
-                ? isAfterFrom || isBeforeTo
-                : isAfterFrom && isBeforeTo;
+            return this._weekdayMatches(now) && 
+                (this.isOvernight ? isAfterFrom || isBeforeTo
+                                  : isAfterFrom && isBeforeTo);
         };
+
+        /**
+         * @param {Date} now
+         */
+        this._weekdayMatches = function (now) {
+            if (this.activeWeekday.includes('all')) {
+                return true;
+            }
+
+            const weekday = now.getDay();
+
+            if (this.activeWeekday.includes('workdays')) {
+                return weekday >= 1 && weekday <= 5;
+            }
+
+            const weekdayName =  ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][weekday];
+
+            return this.activeWeekday.includes(weekdayName);
+        }
 
         /**
          * @param {string} time
